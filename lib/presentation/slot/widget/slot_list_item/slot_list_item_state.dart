3bfaa69213ddcb6b741542d0b80bafd2dd5ce790
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-10-01 02:05:19 +0900

    create factory

--- lib/presentation/slot/widget/slot_list_item/slot_list_item_state.dart
+++ lib/presentation/slot/widget/slot_list_item/slot_list_item_state.dart
@@ -1,15 +1,36 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:random_slot_game/domain/entity/slot/slot.dart';
 
 part 'slot_list_item_state.freezed.dart';
 
 @freezed
 class SlotListItemState with _$SlotListItemState {
-  const factory SlotListItemState({
+  const factory SlotListItemState._({
     required SlotListItemType type,
     required String value,
     required List<String> values,
     @Default(true) isStopped,
   }) = _SlotListItemState;
+
+  factory SlotListItemState.from(Slot slot, SlotListItemType type) {
+    late final List<String> values;
+    switch (type) {
+      case SlotListItemType.player:
+        values = slot.players;
+        break;
+      case SlotListItemType.target:
+        values = slot.targets;
+        break;
+      case SlotListItemType.action:
+        values = slot.actions;
+        break;
+    }
+    return SlotListItemState._(
+      type: type,
+      value: values[0],
+      values: values,
+    );
+  }
 }
 
 enum SlotListItemType {
